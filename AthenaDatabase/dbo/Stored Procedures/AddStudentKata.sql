df20a1844da0964ace8c77b1ae5bbc39c636a213
ScCollins21 <spencer.collins@ndsu.edu>
2025-03-23 16:09:19 -0500

    AddStudentKata proceedure added

---
+++ AthenaDatabase/dbo/Stored Procedures/AddStudentKata.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE [dbo].[AddStudentKata]
+    @StudentID UNIQUEIDENTIFIER,
+    @KataID UNIQUEIDENTIFIER,
+AS
+BEGIN
+    -- Get the UserID associated with the provided StudentID
+    SELECT @UserID UNIQUEIDENTIFIER = UserID FROM dbo.Student WHERE StudentID = @StudentID
+
+    BEGIN
+
+        -- Insert the studentkata record
+        INSERT INTO dbo.[StudentKata] 
+        (StudentID, UserID, KataID, [Complete], [CompleteDate], [CompletionTime], [StudentCode], [StudentNotes], [AdminFeedback]) 
+        VALUES (@StudentID, @UserID, @KataID, 0, NULL, '', '', '', '')
+
+        -- Return the newly inserted studentkata
+        SELECT * FROM dbo.[StudentKata] WHERE KataID = @KataID AND StudentID = @StudentID;
+    END
+END
