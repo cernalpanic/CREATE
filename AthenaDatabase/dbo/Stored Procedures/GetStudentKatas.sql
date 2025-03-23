79be4d0f47e4ebeeb0bfd83c49f068c160ccaf07
ScCollins21 <spencer.collins@ndsu.edu>
2025-03-23 15:23:25 -0500

    GetStudentKatas Proceedure created

---
+++ AthenaDatabase/dbo/Stored Procedures/GetStudentKatas.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE [dbo].[GetStudentKatas]
+	@KataID uniqueidentifier,
+    @StudentID uniqueidentifier
+AS
+BEGIN
+	SELECT K.KataID, K.[Description], K.DateAssigned, K.KataName, 
+        SK.StudentID, SK.Complete, SK.CompleteDate, SK.CompletionTime, SK.StudentCode,
+        SK.StudentNotes, SK.AdminFeedback
+	FROM dbo.[Kata] K
+    RIGHT JOIN dbo.StudentKata SK ON K.KataID = SK.KataID
+	WHERE K.KataID = @KataID AND SK.StudentID = @StudentID
+END
