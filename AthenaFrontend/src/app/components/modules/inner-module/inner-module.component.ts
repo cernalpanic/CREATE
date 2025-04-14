6c98736870e221811f0dd4d839ff8c65c476d51a
stanaha' Fox <xfox2210@gmail.com>
2025-04-13 20:17:50 -0500

    Tabs Reworked and Average Completion
    The tabs for a module have been reworked to show the number of posted and unposted quests inside a module webpage for a mentor. Average Completion has also been implemented, along with the beginnings of the average quests completed being done.

--- AthenaFrontend/src/app/components/modules/inner-module/inner-module.component.ts
+++ AthenaFrontend/src/app/components/modules/inner-module/inner-module.component.ts
@@ -12,9 +12,11 @@ import { EditQuestDialogComponent } from './edit-quest-dialog/edit-quest-dialog'
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { AuthToken } from 'src/models/authtoken.model';
 import { Student } from 'src/models/student.model';
+import { MentorService } from 'src/app/services/mentor.service';
 import { Mentor } from 'src/models/mentor.model';
 import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
 import { EditModuleDialog } from '../edit-module-dialog/edit-module-dialog';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-inner-module',
@@ -25,7 +27,7 @@ export class InnerModuleComponent {
   public module: any;
   public receiveQuests: Quest[] = [];
   public quests: Quest[] = [];
-  private allStudentQuestCompletion: StudentQuest[] = [];
+  protected allStudentQuestCompletion: StudentQuest[] = [];
   private moduleStudentQuest: StudentQuest[] = [];
   protected studentCompleteQuests: StudentQuest[] = [];
   protected studentIncompleteQuests: StudentQuest[] = [];
@@ -42,25 +44,32 @@ export class InnerModuleComponent {
   public allPostedQuests: Quest[] = [];
   public allUnpostedQuests: Quest[] = [];
   protected totalEarnedExp: number = 0;
-
+  protected studentNum = 0;
+  protected totalQuests = 0;
+  protected allStudents: any[] = [];
+  protected moduleCompletedTotal: number = 0;
+  protected averageModuleCompletion: number = 0;
+  protected studentModuleCompletionStatuses: any[] = [];
   constructor(
     public dialog: MatDialog,
     private router: Router,
     public moduleService: ModuleService,
     public questService: QuestService,
+    public mentorService: MentorService,
+    public studentService: StudentService,
     public snackbar: MatSnackBar,
-    public authService: AuthService
-  , public breadcrumb: BreadcrumbService
-    ) {
-      this.initialize();
-      const navigation = this.router.getCurrentNavigation();
-      const state = navigation?.extras.state as {
-        module: Module
-      };
-      this.module = state.module;
-      const pageName: string = this.module.Name + ' Module';
-      breadcrumb.makeCurrentPage(pageName, router.url, state);
-      breadcrumb.setPrevPages();
+    public authService: AuthService,
+    public breadcrumb: BreadcrumbService
+  ) {
+    this.initialize();
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as {
+      module: Module;
+    };
+    this.module = state.module;
+    const pageName: string = this.module.Name + ' Module';
+    breadcrumb.makeCurrentPage(pageName, router.url, state);
+    breadcrumb.setPrevPages();
   }
 
   public async initialize() {
@@ -78,22 +87,60 @@ export class InnerModuleComponent {
     return await this.authService.getAuthentication();
   }
 
+  public async getMentorStudentsNum(id: string): Promise<void> {
+    const response = await this.mentorService.GetMentorStudents(id);
+    if (response) {
+      this.studentNum = response.length;
+    }
+  }
+
+  public async getAllStudents() {
+    const response = await this.studentService.GetAllStudents();
+    this.allStudents = response;
+  }
+
+  public async getAverageCompletionRate() {
+    await this.getAllStudents();
+    for (let i = 0; i < this.allStudents.length; i++) {
+      const response = await this.studentService.GetModuleProgress(
+        this.allStudents[i].roleID,
+        false
+      );
+      this.studentModuleCompletionStatuses = response;
+      for (let j = 0; j < this.studentModuleCompletionStatuses.length; j++) {
+        if (
+          this.studentModuleCompletionStatuses[j].ModuleID ===
+          this.module.ModuleID
+        ) {
+          if (
+            this.studentModuleCompletionStatuses[j].QuestsCompleted ==
+            this.studentModuleCompletionStatuses[j].QuestsAvailable
+          ) {
+            this.moduleCompletedTotal++;
+          }
+        }
+      }
+    }
+    this.averageModuleCompletion = Math.trunc(
+      (this.moduleCompletedTotal / this.allStudents.length) * 100
+    );
+  }
+
   async loadQuestsWithStatus() {
     try {
-      const response = await this.questService.GetAllQuestsWithStatus(this.module.ModuleID);
+      const response = await this.questService.GetAllQuestsWithStatus(
+        this.module.ModuleID
+      );
       this.allPostedQuests = response.posted;
       this.allUnpostedQuests = response.unposted;
 
       this.mentorPostedQuests = this.allPostedQuests.filter(
         (quest) => quest.ModuleID === this.module.ModuleID
-      )
+      );
 
       this.mentorUnpostedQuests = this.allUnpostedQuests.filter(
         (quest) => quest.ModuleID === this.module.ModuleID
-      )
-
-      
-
+      );
     } catch (error) {
       console.error('An error occurred while fetching quests', error);
     }
@@ -102,6 +149,10 @@ export class InnerModuleComponent {
   async ngOnInit() {
     await this.initialize();
     await this.loadQuestsWithStatus();
+    await this.getMentorStudentsNum(this.role.RoleID);
+    await this.getAverageCompletionRate();
+
+    this.totalQuests = this.allPostedQuests.length * this.studentNum;
     const response = await this.getAuthentication();
     this.auth = new AuthToken(response);
     this.role = this.auth.Role;
@@ -134,8 +185,6 @@ export class InnerModuleComponent {
     }
   }
 
-  
-
   //test method for testing
   updateAvailability(questID: string, available: boolean) {
     this.questService.updateQuestAvailability(questID, available).subscribe({
@@ -145,73 +194,99 @@ export class InnerModuleComponent {
       },
       error: (error) => {
         console.error('Error:', error);
-      }
+      },
     });
   }
 
   filterQuests() {
-    if(this.role.Name == 'Student'){
+    if (this.role.Name == 'Student') {
       this.studentFilterQuests();
     }
-    if(this.role.Name == 'Mentor')
-      {
-        this.mentorFilterQuests();
-      }
+    if (this.role.Name == 'Mentor') {
+      this.mentorFilterQuests();
+    }
   }
 
   async calculateTotalEarnedExp() {
     try {
-      const studentQuests = await this.questService.GetStudentQuestCompletion('00000000-0000-0000-0000-000000000000', this.module.ModuleID);
+      const studentQuests = await this.questService.GetStudentQuestCompletion(
+        '00000000-0000-0000-0000-000000000000',
+        this.module.ModuleID
+      );
       if (studentQuests) {
-        this.moduleStudentQuest = studentQuests.map((studentQuest: any) => new StudentQuest(studentQuest));
-        const completedQuests = this.moduleStudentQuest.filter(quest => quest.Completed);
-        console.log("All module's completed quests: ", completedQuests)
-        this.totalEarnedExp = completedQuests.reduce((acc, quest) => acc + quest.ExpGain, 0);
+        this.moduleStudentQuest = studentQuests.map(
+          (studentQuest: any) => new StudentQuest(studentQuest)
+        );
+        const completedQuests = this.moduleStudentQuest.filter(
+          (quest) => quest.Completed
+        );
+        console.log("All module's completed quests: ", completedQuests);
+        this.totalEarnedExp = completedQuests.reduce(
+          (acc, quest) => acc + quest.ExpGain,
+          0
+        );
       } else {
         console.error('Failed to fetch quest completion for student');
       }
     } catch (error) {
-      console.error('An error occurred while fetching quest completion for student', error);
+      console.error(
+        'An error occurred while fetching quest completion for student',
+        error
+      );
     }
   }
 
-  mentorFilterQuests(){
+  mentorFilterQuests() {
     this.calculateTotalEarnedExp();
+    //filtering completed and incomplete quests
+    this.allStudentQuestCompletion.forEach((quest) => {
+      if (quest.Completed) {
+        this.studentCompleteQuests.push(quest);
+      } else {
+        this.studentIncompleteQuests.push(quest);
+      }
+    });
   }
 
-  UpdateStudentVariables(){
+  UpdateStudentVariables() {
     this.numTotalQuests = this.mentorPostedQuests.length;
     this.numCompletedQuests = this.studentCompleteQuests.length;
   }
 
-/**
- * Asynchronously filters and categorizes quests based on their completion status
- * for a specific student. This method does the following:
- *
- * 1. Fetches all quest completion records as `StudentQuest` objects for the student identified by their student ID.
- * 2. Maps the received data to instances of `StudentQuest` and stores them in `this.allStudentQuestCompletion`.
- * 3. Iterates over all `allStudentQuestcompletion` and categorizes each quest into either `studentCompleteQuests` or `studentIncompleteQuests`
- *    based on the completion status found in `allStudentQuestCompletion`.
- *
- */
+  /**
+   * Asynchronously filters and categorizes quests based on their completion status
+   * for a specific student. This method does the following:
+   *
+   * 1. Fetches all quest completion records as `StudentQuest` objects for the student identified by their student ID.
+   * 2. Maps the received data to instances of `StudentQuest` and stores them in `this.allStudentQuestCompletion`.
+   * 3. Iterates over all `allStudentQuestcompletion` and categorizes each quest into either `studentCompleteQuests` or `studentIncompleteQuests`
+   *    based on the completion status found in `allStudentQuestCompletion`.
+   *
+   */
   private async studentFilterQuests() {
-
     //populating array of all quest completion details of this particular student
     try {
-      const studentQuests = await this.questService.GetStudentQuestCompletion(this.role.RoleID, this.module.ModuleID);
+      const studentQuests = await this.questService.GetStudentQuestCompletion(
+        this.role.RoleID,
+        this.module.ModuleID
+      );
       if (studentQuests) {
-        this.allStudentQuestCompletion = studentQuests.map((studentQuest: any) =>
-          new StudentQuest(studentQuest));
+        this.allStudentQuestCompletion = studentQuests.map(
+          (studentQuest: any) => new StudentQuest(studentQuest)
+        );
       } else {
         console.error('Failed to fetch quest completion for student');
       }
     } catch (error) {
-      console.error('An error occurred while fetching quest completion for student', error);
+      console.error(
+        'An error occurred while fetching quest completion for student',
+        error
+      );
     }
 
     //filtering completed and incomplete quests
-    this.allStudentQuestCompletion.forEach(quest => {
-      if (quest.Completed){
+    this.allStudentQuestCompletion.forEach((quest) => {
+      if (quest.Completed) {
         this.totalExp += quest.ExpGain;
         this.currentExp += quest.ExpGain;
         this.studentCompleteQuests.push(quest);
@@ -222,7 +297,6 @@ export class InnerModuleComponent {
     });
     this.UpdateStudentVariables();
   }
-
 
   public getImgLink(name: string) {
     return '../../../../assets/images/' + name.toLowerCase() + 'logo.png';
@@ -274,7 +348,7 @@ export class InnerModuleComponent {
     dialogRef.afterClosed().subscribe((response) => {
       if (response) {
         let newQuest = new Quest(response);
-        if(quest.Available){
+        if (quest.Available) {
           this.mentorPostedQuests.splice(index, 1, newQuest);
         } else {
           this.mentorUnpostedQuests.splice(index, 1, newQuest);
